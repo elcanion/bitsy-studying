63f7e8b51e3dfe9075c0cda783865eaaba8fc2f0
elcanion <elcaniondev@gmail.com>
2024-05-14 08:12:05 -0300

    added a tiny GET request

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BitsyCompComponent } from './bitsy-comp/bitsy-comp.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BitsyCompComponent],
+  imports: [RouterOutlet, BitsyCompComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent{
+  
   title = 'bitsy-studying';
   subtitle = 'just a little bitsy thing'
 }
--- front/src/app/bitsy-comp/bitsy-comp.component.ts
+++ front/src/app/bitsy-comp/bitsy-comp.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bitsy-comp',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './bitsy-comp.component.html',
   styleUrl: './bitsy-comp.component.css'
 })
-export class BitsyCompComponent {
+export class BitsyCompComponent implements OnInit{
+  totalAngularPackages: any;
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.http.get<any>('https://api.npms.io/v2/search?q=scope:angular').subscribe(data => {
+      this.totalAngularPackages = data.total;
+    })
+  }
 }
